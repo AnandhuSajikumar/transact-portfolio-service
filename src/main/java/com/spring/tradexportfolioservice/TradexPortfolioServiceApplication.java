8ef07a629f6fb503be18bafc00d542512e489648
anandhu <anandhuzz916@gmail.com>
2026-02-19 14:30:48 +0530

    db setup -2

--- src/main/java/com/spring/tradexportfolioservice/TradexPortfolioServiceApplication.java
+++ src/main/java/com/spring/tradexportfolioservice/TradexPortfolioServiceApplication.java
@@ -3,10 +3,13 @@ package com.spring.tradexportfolioservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TradexPortfolioServiceApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
         SpringApplication.run(TradexPortfolioServiceApplication.class, args);
     }
 
